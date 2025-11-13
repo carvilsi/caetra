f0854044b1ad6801442bbc75f0f3ba559c14ca3e
carvilsi <carvilsi.mail.list@gmail.com>
2025-11-13 15:56:56 +0100

    testing shield-gen

--- tools/caetra_shield_generator/caetra_bpf_generator.py
+++ tools/caetra_shield_generator/caetra_bpf_generator.py
@@ -1,20 +1,103 @@
+#!/usr/bin/env python
+
+import click
 import os
 from jinja2 import Environment, FileSystemLoader
 
-# loading the environment
-env = Environment(loader = FileSystemLoader("templates"))
+FOLDER_TEMPLATE = "templates"
+C_TEMPLATE = "ctr_bpf_c.jinja"
+PYTHON_TEMPLATE = "ctr_bpf_py.jinja"
+TOML_TEMPLATE = "ctr_bpf_toml.jinja"
 
-# loading the template
-template = env.get_template("ctr_bpf_py.jinja")
+def write_template_output(shield_name, file_extension, content):
+    with open(f"{shield_name}/{shield_name}.{file_extension}", "w", encoding="utf-8") as f:
+        f.write(content)
 
-# rendering the template and storing the resultant text in variable output
-output = template.render(shield_name = "test", kprobe_event = "foo")
+def prompt_canarytoken(ctx, param, canarytoken_sender):
+    if canarytoken_sender:
+        token = ctx.params.get('canarytoken')
+        if not token:
+            token = click.prompt('Canary DNS token')
 
-# printing the output on screen
-print(output)
+        return token
 
-if not os.path.exists("test"):
-    os.makedirs("test")
 
-with open("test/test.py", "w", encoding="utf-8") as f:
-    f.write(output)
+def prompt_telegram(ctx, param, telegram_sender):
+    if telegram_sender:
+        chatid = ctx.params.get('telegram_chat_id')
+        if not chatid:
+            chatid = click.prompt('Telegram chat_id', type=click.INT)
+
+        botapikey = ctx.params.get('telegram_bot_api_key')
+        if not botapikey:
+            botapikey = click.prompt('Telegram bot_api_key', type=click.STRING)
+
+        return (chatid, botapikey) 
+
+
+@click.command()
+@click.option("--shield-name", type=click.STRING, required=True, prompt="Shield name", help="New caetra shield name.")
+@click.option("--shield-description", type=click.STRING, required=False, prompt="Shield description", default="None", help="Write down about what this Shield does.")
+@click.option("--kprobe-event", type=click.STRING, required=True, prompt="Kprobe event", help="Kprobe event.")
+@click.option("--c-function-name", type=click.STRING, required=True, prompt="Function name", help="Name function for c code.")
+@click.option("--kheaders-include", type=click.STRING, required=True, prompt="Linux header to include", help="Linux header to includ on kernel c code, e.g. 'linux/usb.h'")
+@click.option("--kstrct", type=click.STRING, required=True, prompt="Related kprobe related struct to get data", help="Linux struct to retrieve data, e.g. 'usb_device'")
+@click.option("--shield-enable", type=click.BOOL, required=True, default="y", prompt="Is this Shield enabled?", help="If this Shield will be enabled or not")
+@click.option("--shield-feature", type=click.STRING, required=False, default="None", prompt="Name of the feature if is has one", help="Will be the feature variable for the Shield.")
+@click.option('--canarytoken-sender', prompt="Should this Shield send notifications with Cananry tokens?", is_flag=True, default=True, callback=prompt_canarytoken)
+@click.option('--canarytoken', is_eager=True, type=click.STRING)
+@click.option('--telegram-sender', prompt="Should this Shield send notifications with Telegram Bot?", is_flag=True, default=True, callback=prompt_telegram)
+@click.option('--telegram-chat-id', is_eager=True)
+@click.option('--telegram-bot-api-key', is_eager=True)
+
+
+def caetra_shield_generator(shield_name, shield_description, kprobe_event, c_function_name, kheaders_include, kstrct, shield_enable, shield_feature, canarytoken_sender, canarytoken, telegram_sender, telegram_chat_id, telegram_bot_api_key):
+    shield_name = shield_name.replace(" ", "_")
+    c_function_name = c_function_name.replace(" ", "_")
+    
+    if canarytoken_sender:
+        canarytoken = canarytoken_sender
+        canarytoken_sender = True
+    
+    if telegram_sender:
+        telegram_chat_id = telegram_sender[0]
+        telegram_bot_api_key = telegram_sender[1]
+        telegram_sender = True
+
+    print(f"shield_name: {shield_name}")
+    print(f"shield_description: {shield_description}")
+    print(f"kprobe_event: {kprobe_event}")
+    print(f"c_function_name: {c_function_name}")
+    print(f"kheaders_include: {kheaders_include}")
+    print(f"kstrct: {kstrct}")
+    print(f"shield_enable: {shield_enable}")
+    print(f"shield_feature: {shield_feature}")
+    print(f"canarytoken_sender: {canarytoken_sender}")
+    print(f"canarytoken: {canarytoken}")
+    print(f"telegram_sender: {telegram_sender}")
+    print(f"telegram_chat_id: {telegram_chat_id}")
+    print(f"telegram_bot_api_key: {telegram_bot_api_key}")
+
+    if not os.path.exists(shield_name):
+        os.makedirs(shield_name)
+    
+    env = Environment(loader = FileSystemLoader(FOLDER_TEMPLATE))
+    
+    # Python Template
+    python_template = env.get_template(PYTHON_TEMPLATE)
+    python_output = python_template.render(shield_name, kprobe_event, c_function_name, shield_feature)
+    write_template_output(shield_name, "py", python_output)    
+    
+    # C Kernel side Template
+    c_template = env.get_template(C_TEMPLATE)
+    c_output = c_template.render(shield_name, kheaders_include, kstrct)
+    write_template_output(shield_name, "c", c_output)    
+    
+    # Toml Template
+    toml_template = env.get_template(TOML_TEMPLATE)
+    toml_output = toml_template.render(shield_name, shield_description, shield_feature, canarytoken_sender, canarytoken, telegram_sender, telegram_chat_id, telegram_bot_api_key)
+    write_template_output(shield_name, "toml", toml_output)    
+
+if __name__ == '__main__':
+    caetra_shield_generator()
+
--- tools/caetra_shield_generator/cli.py
+++ tools/caetra_shield_generator/cli.py
@@ -1,24 +1,74 @@
+#!/usr/bin/env python
+
 import click
 
+def prompt_canarytoken(ctx, param, canarytoken_sender):
+    if canarytoken_sender:
+        token = ctx.params.get('canarytoken')
+        if not token:
+            token = click.prompt('Canary DNS token')
+
+        return token
+
+
+def prompt_telegram(ctx, param, telegram_sender):
+    if telegram_sender:
+        chatid = ctx.params.get('telegram_chat_id')
+        if not chatid:
+            chatid = click.prompt('Telegram chat_id', type=click.INT)
+
+        botapikey = ctx.params.get('telegram_bot_api_key')
+        if not botapikey:
+            botapikey = click.prompt('Telegram bot_api_key', type=click.STRING)
+
+        return (chatid, botapikey) 
+
+
 @click.command()
-@click.option("--shield_name", prompt="Shield name",
-              help="New caetra shield name.")
-# TODO: would it be nice to check if the kprobe_event exists
-@click.option("--kprobe_event", prompt="Kprobe event",
-              help="Kprobe event.")
-@click.option("--c_function_name", prompt="Function name",
-              help="Name function for c code.")
-
-def caetra_shield_gen(shield_name, kprobe_event, c_function_name):
-    """caetra bpf shield generator."""
-
-    # we do not want spaces on Shields name
-    shield_name = shield_name.replace(" ", "_") 
-
-    print(shield_name)
-    print(kprobe_event)
-    print(c_function_name)
+@click.option("--shield-name", type=click.STRING, required=True, prompt="Shield name", help="New caetra shield name.")
+@click.option("--shield-description", type=click.STRING, required=False, prompt="Shield description", default="None", help="Write down about what this Shield does.")
+@click.option("--kprobe-event", type=click.STRING, required=True, prompt="Kprobe event", help="Kprobe event.")
+@click.option("--c-function-name", type=click.STRING, required=True, prompt="Function name", help="Name function for c code.")
+@click.option("--kheaders-include", type=click.STRING, required=True, prompt="Linux header to include", help="Linux header to includ on kernel c code, e.g. 'linux/usb.h'")
+@click.option("--kstrct", type=click.STRING, required=True, prompt="Related kprobe related struct to get data", help="Linux struct to retrieve data, e.g. 'usb_device'")
+@click.option("--shield-enable", type=click.BOOL, required=True, default="y", prompt="Is this Shield enabled?", help="If this Shield will be enabled or not")
+@click.option("--shield-feature", type=click.STRING, required=False, default="None", prompt="Name of the feature if is has one", help="Will be the feature variable for the Shield.")
+@click.option('--canarytoken-sender', prompt="Should this Shield send notifications with Cananry tokens?", is_flag=True, default=True, callback=prompt_canarytoken)
+@click.option('--canarytoken', is_eager=True, type=click.STRING)
+@click.option('--telegram-sender', prompt="Should this Shield send notifications with Telegram Bot?", is_flag=True, default=True, callback=prompt_telegram)
+@click.option('--telegram-chat-id', is_eager=True)
+@click.option('--telegram-bot-api-key', is_eager=True)
 
 
-if __name__ == "__main__":
-    caetra_shield_gen()
+def cli(shield_name, shield_description, kprobe_event, c_function_name, kheaders_include, kstrct, shield_enable, shield_feature, canarytoken_sender, canarytoken, telegram_sender, telegram_chat_id, telegram_bot_api_key):
+    shield_name = shield_name.replace(" ", "_")
+    c_function_name = c_function_name.replace(" ", "_")
+    
+    if canarytoken_sender:
+        canarytoken = canarytoken_sender
+        canarytoken_sender = True
+    
+    if telegram_sender:
+        telegram_chat_id = telegram_sender[0]
+        telegram_bot_api_key = telegram_sender[1]
+        telegram_sender = True
+
+    print(f"shield_name: {shield_name}")
+    print(f"shield_description: {shield_description}")
+    print(f"kprobe_event: {kprobe_event}")
+    print(f"c_function_name: {c_function_name}")
+    print(f"kheaders_include: {kheaders_include}")
+    print(f"kstrct: {kstrct}")
+    print(f"shield_enable: {shield_enable}")
+    print(f"shield_feature: {shield_feature}")
+    print(f"canarytoken_sender: {canarytoken_sender}")
+    print(f"canarytoken: {canarytoken}")
+    print(f"telegram_sender: {telegram_sender}")
+    print(f"telegram_chat_id: {telegram_chat_id}")
+    print(f"telegram_bot_api_key: {telegram_bot_api_key}")
+
+if __name__ == '__main__':
+    cli()
+
+
+
